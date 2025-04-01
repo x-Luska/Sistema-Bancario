2f12cd9cea2fdd2bcc2732c62875ee7493cdb6c2
Lucas Gonçalves <lucas.dias333@outlook.com>
2025-04-01 00:58:21 -0300

    Create UsuarioContaBacaria.java
    Armazena dados do usuário

---
+++ UsuarioContaBacaria.java
@@ -0,0 +1,13 @@
+class UsuarioContaBancaria {
+    private String nome;
+    private String cpf;
+
+    public UsuarioContaBancaria(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
